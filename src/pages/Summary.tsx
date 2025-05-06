217de86f440ff68f5dbfd88ecd68b59ee2a7e491
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-05-06 10:25:07 -0300

    Update Summary.tsx

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -93,9 +93,10 @@ export default function Summary() {
               <ResponsiveContainer width="100%" height="100%">
                 <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 20 }}>
                   <XAxis dataKey="week" stroke="#fff" />
-                 <YAxis
+<YAxis
   stroke="#fff"
   domain={[10, 90]}
+  tick={false}
 >
   <Label
     value="Consumo de Álcool"
@@ -160,9 +161,10 @@ export default function Summary() {
               <ResponsiveContainer width="100%" height="100%">
                 <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 20 }}>
                   <XAxis dataKey="week" stroke="#fff" />
-                 <YAxis
+                <YAxis
   stroke="#fff"
   domain={[10, 90]}
+  tick={false}
 >
   <Label
     value="Consumo de Álcool"
